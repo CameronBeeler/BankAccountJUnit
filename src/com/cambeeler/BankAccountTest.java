9df4213b05f6d698740410c72b37317c91bf35f3
Cameron Beeler <cameron.beeler@gmail.com>
2019-02-19 16:59:04 -0800

    adding some JUnit method types

--- src/com/cambeeler/BankAccountTest.java
+++ src/com/cambeeler/BankAccountTest.java
@@ -1,31 +1,56 @@
 package com.cambeeler;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankAccountTest
 {
+    private BankAccount camAccount;
+    private static int testMethodCounter=0;
+
+    @BeforeAll
+    public static void beforeClass()
+    {
+        System.out.println("This executes before any test classes");
+        System.out.println(testMethodCounter++);
+    }
+
+
+    @BeforeEach
+    public void setup()
+    {
+        camAccount = new BankAccount("Cameron", "Beeler", 1_000.0, BankAccount.CHECKING);
+        System.out.println("Running a BankAccount test");
+        System.out.println(testMethodCounter++);
+
+    }
 
     @Test
     void deposit()
     {
-        BankAccount camAccount = new BankAccount("Cameron", "Beeler", 1_000.0, 2);
         double balance = camAccount.deposit(200.00, true);
         double variance = 0;
         assertEquals(1_200, balance, variance);
         balance = camAccount.deposit(-200.0, false);
         assertEquals(1_200, balance, variance);
+        System.out.println(testMethodCounter++);
     }
 
     @Test
     void withdrawal()
     {
-        BankAccount camAccount = new BankAccount("Cameron", "Beeler", 950.0, 1);
         double balance = camAccount.withdrawal(750.0, true);
         double variance = 0;
-        assertEquals(200, balance);
+        assertEquals(250, balance);
         balance = camAccount.withdrawal(500, false);
+        assertEquals(250, balance);
+        balance = camAccount.withdrawal(-200, false);
+        assertEquals(250, balance);
+        System.out.println(testMethodCounter++);
 
     }
 
@@ -34,40 +59,33 @@ class BankAccountTest
     {
         double variance = 0.03;
         double balance = 1250.37;
-        BankAccount camsAccount = new BankAccount("Cameron", "Beeler", balance, 1);
-        balance = camsAccount.deposit(175.98, false);
-        assertEquals(balance, camsAccount.getBalance(), variance);
+        balance = camAccount.deposit(175.98, false);
+        assertEquals(balance, camAccount.getBalance(), variance);
+        System.out.println(testMethodCounter++);
     }
 
     @Test
     void getBalance_withdrawal()
     {
         double variance = 0.03;
-        double balance = 1250.37;
-        BankAccount camsAccount = new BankAccount("Cameron", "Beeler", balance, 1);
-        balance = camsAccount.withdrawal(250.73, false);
-        assertEquals(balance, camsAccount.getBalance(), variance);
-    }
-
-    @Test
-    void imaFailedTest()
-    {
-        fail("because that is in my nature");
+        double balance = camAccount.withdrawal(250.73, false);
+        assertEquals(balance, camAccount.getBalance(), variance);
+        System.out.println(testMethodCounter++);
     }
 
     @Test
     void isCheckingTest()
     {
-        double balance = 2237.85;
-        BankAccount ba = new BankAccount("Cam", "Beeler", balance, 0);
-        BankAccount ba2 = new BankAccount("Cam", "Beeler", balance, 3);
-        BankAccount ba3 = new BankAccount("Cam", "Beeler", balance, 1);
-        BankAccount ba4 = new BankAccount("Cam", "Beeler", balance, 2);
+        assertTrue(camAccount.isChecking());
 
-        assertEquals(true, (ba.isChecking()));
-        assertEquals(true, (ba2.isChecking()));
-        assertEquals(true, (ba3.isChecking()));
-        assertEquals(false, (ba4.isChecking()));
+        System.out.println(testMethodCounter++);
     }
 
+    @AfterAll
+    public static void afterClass()
+    {
+        System.out.println("This executes after all test classes");
+        System.out.println(testMethodCounter++);
+
+    }
 }
